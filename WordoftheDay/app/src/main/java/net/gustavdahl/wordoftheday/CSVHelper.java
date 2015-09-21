22c98d1336b6a092a92000ff88547ea84f21de47
Wikzo <Linkthewindwaker@gmail.com>
2015-09-21 18:55:00 +0200

    savedata() almost working (but appends ...)

--- WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/CSVHelper.java
+++ WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/CSVHelper.java
@@ -1,10 +1,16 @@
 package net.gustavdahl.wordoftheday;
 
 
+import android.os.Environment;
 import android.util.Log;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,7 +20,7 @@ import java.util.List;
 public class CSVHelper
 {
 
-    public static List<Word> GetAllData(InputStream in, int row, int column)
+    public static List<Word> ReadCSVFile(InputStream in, int numberOfRows, int numberOfColumns)
     {
         String next[] = {};
         List<String[]> list = new ArrayList<String[]>();
@@ -44,14 +50,11 @@ public class CSVHelper
         int columnMax = list.get(0).length;
 
 
-        if (row != 0)
-            rowMax = row;
+        if (numberOfRows != 0)
+            rowMax = numberOfRows;
 
-        if (column != 0)
-            columnMax = column;
-
-        String data = "";
-
+        if (numberOfColumns != 0)
+            columnMax = numberOfColumns;
 
         List<Word> words = new ArrayList<Word>();
 
@@ -62,20 +65,43 @@ public class CSVHelper
             for (int j = 0; j < columnMax; j++)
             {
                 t += list.get(i)[j] + ";";
-                //data += list.get(i)[j];
-
             }
 
             //Log.d(MainActivity.TAG, t);
             Word w = new Word(i, t, ";");
             words.add(w);
-
-           //Log.d(MainActivity.TAG, w.ToString(true));
-
-
         }
 
         return words;
 
     }
+
+    private static final char SEPARATOR = ';';
+    public static void UpdateCSV(InputStream input, String  replace, int row, int col) throws IOException
+    {
+
+        File sdCard = Environment.getExternalStorageDirectory();
+        File dir = new File (sdCard.getAbsolutePath() + "/dir");
+
+        File file = new File(dir, "tsxt.txt");
+
+        FileOutputStream f = new FileOutputStream(file);
+
+
+
+        //////////
+
+        CSVReader reader = new CSVReader(new InputStreamReader(input));
+
+        List<String[]> csvBody = reader.readAll();
+        csvBody.get(row)[col]=replace;
+        reader.close();
+
+        CSVWriter writer = new CSVWriter(new FileWriter(file),SEPARATOR,' ');
+        writer.writeAll(csvBody);
+        writer.flush();
+        writer.close();
+    }
+
+
 }
--- WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/MainActivity.java
+++ WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/MainActivity.java
@@ -1,5 +1,7 @@
 package net.gustavdahl.wordoftheday;
 
+import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +12,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,9 +58,278 @@ public class MainActivity extends AppCompatActivity
 
         WordList = new ArrayList<Word>();
 
-        LoadCSVData();
+
+        try
+        {
+            saveText();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
 
+        //LoadCSVData();
+
+
+    }
+
+    void InternalAGain()
+    {
+        // Create a file in the Internal Storage
+
+        String fileName = "MyFile";
+        String content = "hello world";
+
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            outputStream.write(content.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        // external
+    }
+
+    void ExternalAGain()
+    {
+        String content = "hello world";
+        File file;
+        FileOutputStream outputStream;
+        try {
+            file = new File(Environment.getExternalStorageDirectory(), "MyCache");
+
+            outputStream = new FileOutputStream(file);
+            outputStream.write(content.getBytes());
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static final char SEPARATOR = ';';
+
+    private static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    void WriteInternal()
+    {
+        String filename = "myfile.csv";
+        String string = "Hello world!";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void WriteCSV() throws IOException
+    {
+
+        boolean b = isExternalStorageAvailable();
+        Log.d(TAG, String.valueOf(b));
+
+        String text = "JA DA JA DA";
+        String FILE_NAME = "file.txt";
+        if (isExternalStorageAvailable() && isExternalStorageReadOnly()) {
+            String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+            File file = new File(baseDir, FILE_NAME);
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(file);
+                writer.write(text.toString());
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /*
+
+        String sdpath, sd1path, usbdiskpath, sd0path;
+        if (new File("/storage/extSdCard/").exists()) {
+            sdpath="/storage/extSdCard/";
+            Log.i("Sd Cardext Path", sdpath);
+        }
+        if (new File("/storage/sdcard1/").exists()) {
+            sd1path="/storage/sdcard1/";
+            Log.i("Sd Card1 Path", sd1path);
+        }
+        if (new File("/storage/usbcard1/").exists()) {
+            usbdiskpath="/storage/usbcard1/";
+            Log.i("USB Path", usbdiskpath);
+        }
+        if (new File("/storage/sdcard0/").exists()) {
+            sd0path="/storage/sdcard0/";
+            Log.i("Sd Card0 Path", sd0path);
+        }
+
+        //
+
+        String csv = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+        CSVWriter writer = new CSVWriter(new FileWriter(csv));
+
+        List<String[]> data = new ArrayList<String[]>();
+        data.add(new String[] {"India", "New Delhi"});
+        data.add(new String[] {"United States", "Washington D.C"});
+        data.add(new String[] {"Germany", "Berlin"});
+
+        writer.writeAll(data);
+
+        writer.close();*/
+    }
+
+
+    public void saveText() throws IOException
+    {
+        // text file: https://stackoverflow.com/questions/27415074/android-save-to-file-txt-appending
+        try {
+            //open file for writing
+            OutputStreamWriter out1 = new OutputStreamWriter(openFileOutput("save.csv", MODE_PRIVATE));
+            out1.write("Word1,Meaning1,UsedCounter1,NextDate1,AddedDate1,Notes1,Category1,Language1");
+            out1.close();
+
+
+            Toast.makeText(this, out1.toString() ,Toast.LENGTH_LONG).show();
+
+
+            OutputStreamWriter out = new OutputStreamWriter(openFileOutput("save.csv", Context.MODE_APPEND));
+            //OutputStreamWriter out = new OutputStreamWriter(openFileOutput("save.csv", MODE_PRIVATE));
+
+
+            // IN CSV:
+            InputStreamReader input = new InputStreamReader(openFileInput("save.csv"));
+            CSVReader reader = new CSVReader(input);
+            List<String[]> csvBody = reader.readAll();
+            csvBody.get(0)[0]="WordREPLACED";
+            csvBody.get(0)[1]="MeaningREPLACED";
+            reader.close();
+
+
+
+            // OUT CSV:
+            CSVWriter writer = new CSVWriter(out, ',');
+            writer.writeAll(csvBody);
+            writer.flush();
+            writer.close();
+            ///
+
+
+            //write information to file
+            //String text2 = "HJSA SAD ASD SD";
+            //out.write(text2);
+            //out.write('\n');
+
+            //close file
+
+            out.close();
+
+
+            Toast.makeText(this,"Text Saved",Toast.LENGTH_LONG).show();
+
+        } catch (java.io.IOException e) {
+            //if caught
+            Toast.makeText(this, "Text Could not be added",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+void WriteNewCSVFile()
+{
+    File folder = new File(Environment.getExternalStorageDirectory()
+            + "/Folder");
+
+    boolean var = false;
+    if (!folder.exists())
+        var = folder.mkdir();
+
+    System.out.println("" + var);
+
+
+    final String filename = folder.toString() + "/" + "Test.csv";
+
+
+}
+
+
+    public void updateCSV(String replace, int row, int col) throws IOException {
+
+        String filename = "CasdasdSVStuffmyfile.csv";
+        String string = "Hello world!asdasdasd";
+        FileOutputStream outputStream;
+
+
+        InputStream in = getAssets().open("csvTest.csv");
+        CSVReader reader = new CSVReader(new InputStreamReader(in));
+
+
+        CSVWriter writer = new CSVWriter(new FileWriter("yourfile.csv"), '\t');
+        // feed in your array (or convert your data to an array)
+        String[] entries = "first#second#third".split("#");
+        writer.writeNext(entries);
+        writer.close();
+
+
+        /// ORIGINAL:
+
+        List<String[]> csvBody = reader.readAll();
+        csvBody.get(row)[col]=replace;
+        reader.close();
+
+        /*//CSVWriter writer = new CSVWriter(new FileWriter(output),SEPARATOR,' ');
+        writer.writeAll(csvBody);
+        writer.flush();
+        writer.close();*/
+
+
+        // mine:
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+    void UpdateCSV()
+    {
+        try
+        {
+            InputStream input = getAssets().open("csvTest.csv");
+
+
+            CSVHelper.UpdateCSV(input, "lol", 1, 1);
+
+
+            //Log.d(TAG,s);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public void LoadCSVData()
@@ -57,7 +339,7 @@ public class MainActivity extends AppCompatActivity
         {
             InputStream input = getAssets().open("csvTest.csv");
 
-            List<Word> w = CSVHelper.GetAllData(input, 3, 0);
+            List<Word> w = CSVHelper.ReadCSVFile(input, 3, 0);
 
             Log.d(TAG,w.get(1).ToString(true));
 
