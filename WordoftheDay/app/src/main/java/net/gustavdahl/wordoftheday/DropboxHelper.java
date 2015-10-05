0c9ab9bfb0733d65377a317cdb4c3c52440404e4
Wikzo <Linkthewindwaker@gmail.com>
2015-10-05 20:26:16 +0200

    Upload/download + Dropbox stored token

--- WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/DropboxHelper.java
+++ WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/DropboxHelper.java
@@ -1,8 +1,13 @@
 package net.gustavdahl.wordoftheday;
 
+import android.content.Context;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
 import com.dropbox.client2.exception.DropboxException;
 
 import java.io.File;
@@ -16,9 +21,23 @@ import java.io.UnsupportedEncodingException;
 
 public class DropboxHelper
 {
-    public static void UploadFile(final InputStream input, final boolean useCSV)
+    private static MainActivity parent;
+    private static DropboxAPI<AndroidAuthSession> dropbox;
+
+    /*public DropboxHelper(DropboxAPI<AndroidAuthSession> db)
     {
-        if (MainActivity.mDBApi.getSession().authenticationSuccessful())
+        dropbox = db;
+    }*/
+
+
+    public void UploadFile(final String fileName, final MainActivity parent, DropboxAPI<AndroidAuthSession> db)
+    {
+        this.parent = parent;
+        this.dropbox = db;
+
+
+        //if (dropbox.getSession().authenticationSuccessful())
+        if (true)
         {
             Thread thread = new Thread(new Runnable()
             {
@@ -28,43 +47,37 @@ public class DropboxHelper
                     try
                     {
                         File root = android.os.Environment.getExternalStorageDirectory();
-                        File file = new File(root.getAbsolutePath() + "/db_test.txt");
+                        File file = new File(root.getAbsolutePath() + "/" + fileName);
                         FileInputStream inputStream = new FileInputStream(file);
 
 
+                        DropboxAPI.Entry response = dropbox.putFileOverwrite("/" + fileName, inputStream, file.length(), null);
 
-                        PrintWriter writer = null;
-                        try
+                        //Log.i("DbExampleLog", "The uploaded file's rev is: " + response.rev);
+                        Log.i(MainActivity.TAG, "Dropbox successfully uploaded!");
+
+
+                        parent.runOnUiThread(new Runnable()
                         {
-                            writer = new PrintWriter(root.getAbsolutePath() + "/db_test.txt", "UTF-8");
-                        } catch (FileNotFoundException e)
-                        {
-                            e.printStackTrace();
-                        } catch (UnsupportedEncodingException e)
-                        {
-                            e.printStackTrace();
-                        }
-                        writer.println(System.currentTimeMillis());
-                        writer.close();
+                            public void run()
+                            {
+                                Toast.makeText(parent.getBaseContext(), "Upload successful", Toast.LENGTH_SHORT).show();
+                            }
+                        });
 
-
-
-                        if (useCSV) // asset
-                        {
-                            DropboxAPI.Entry response = MainActivity.mDBApi.putFileOverwrite("/csv_text.txt", input, input.available(), null);
-                            Log.i("DbExampleLog", "The uploaded file's rev is: " + response.rev);
-
-                        }
-                        else // new file
-                        {
-                            DropboxAPI.Entry response = MainActivity.mDBApi.putFileOverwrite("/db_test.txt", inputStream, file.length(), null);
-                            Log.i("DbExampleLog", "The uploaded file's rev is: " + response.rev);
-
-                        }
 
                     } catch (Exception e)
                     {
                         e.printStackTrace();
+
+                        parent.runOnUiThread(new Runnable()
+                        {
+                            public void run()
+                            {
+                                Toast.makeText(parent.getBaseContext(), "Upload unsuccessful", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
                     }
                 }
             });
@@ -72,13 +85,16 @@ public class DropboxHelper
         }
     }
 
-    public static void DownloadFile(final String filePath) throws FileNotFoundException
+    // https://stackoverflow.com/questions/28254321/android-dropbox-core-api-upload-file-sample
+    public void DownloadFile(final String fileName, final MainActivity parent, DropboxAPI<AndroidAuthSession> db)
     {
+        this.parent = parent;
+        this.dropbox = db;
 
-
-        if (MainActivity.mDBApi.getSession().authenticationSuccessful())
+        //if (dropbox.getSession().authenticationSuccessful())
+        if (true)
         {
-            Thread thread = new Thread(new Runnable()
+            final Thread thread = new Thread(new Runnable()
             {
                 @Override
                 public void run()
@@ -86,29 +102,58 @@ public class DropboxHelper
                     try
                     {
                         File root = Environment.getExternalStorageDirectory();
-                        File file= new File (root.getAbsolutePath() + filePath);
-                        try {
+                        File file = new File(root.getAbsolutePath() + "/" + fileName);
+                        try
+                        {
                             FileOutputStream f = new FileOutputStream(file);
 
-                            DropboxAPI.DropboxFileInfo info = MainActivity.mDBApi.getFile(filePath, null, f, null);
-                            Log.i("DbExampleLog", "The file's rev is: " + info.getMetadata().rev);
+                            DropboxAPI.DropboxFileInfo info = dropbox.getFile("/" + fileName, null, f, null);
+
+                            //Log.i("DbExampleLog", "The file's rev is: " + info.getMetadata().rev);
+                            Log.i(MainActivity.TAG, "Dropbox downloaded successfully!");
+
+                            parent.runOnUiThread(new Runnable()
+                            {
+                                public void run()
+                                {
+                                    Toast.makeText(parent.getBaseContext(), "Download successful", Toast.LENGTH_SHORT).show();
+                                }
+                            });
 
 
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
+                        } catch (FileNotFoundException e)
+                        {
                             Log.i(MainActivity.TAG, "******* File not found. Did you" +
-                                    " add a WRITE_EXTERNAL_STORAGE permission to the   manifest?");
-                        } catch (IOException e) {
+                                    " add a WRITE_EXTERNAL_STORAGE permission to the manifest?");
+
                             e.printStackTrace();
+
+                        } catch (IOException e)
+                        {
+                            Log.i(MainActivity.TAG, "IO exception!");
+                            e.printStackTrace();
+
+
                         } catch (DropboxException e)
                         {
+                            Log.i(MainActivity.TAG, "Dropbox exception!");
                             e.printStackTrace();
+
+                            parent.runOnUiThread(new Runnable()
+                            {
+                                public void run()
+                                {
+                                    Toast.makeText(parent.getBaseContext(), "Download unsuccessful", Toast.LENGTH_SHORT).show();
+                                }
+                            });
                         }
 
 
                     } catch (Exception e)
                     {
                         e.printStackTrace();
+                        Log.i(MainActivity.TAG, "Stack exception!");
+
                     }
                 }
             });
--- WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/MainActivity.java
+++ WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/MainActivity.java
@@ -1,35 +1,27 @@
 package net.gustavdahl.wordoftheday;
 
-import android.content.res.AssetManager;
-import android.os.Environment;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
 import com.dropbox.client2.exception.DropboxException;
-import com.dropbox.client2.exception.DropboxUnlinkedException;
+import com.dropbox.client2.session.AccessTokenPair;
 import com.dropbox.client2.session.AppKeyPair;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -37,17 +29,28 @@ public class MainActivity extends AppCompatActivity
 
     //private static final String TAG = MainActivity.class.getSimpleName();
     public static final String TAG = "MyDebug";
+    private static final String ACCOUNT_PREFS_NAME = "";
 
     CSVAdapter csvAdapter;
 
 
-    public static DropboxAPI<AndroidAuthSession> mDBApi;
+    private static DropboxAPI<AndroidAuthSession> mDBApi;
+    public DropboxAPI<AndroidAuthSession> GetDB()
+    {
+        if (mDBApi == null)
+        {
+            mDBApi = getDropboxAPI();
+        }
+        return mDBApi;
+    }
     final static private String APP_KEY = "akbycb3wan8az7a";
     final static private String APP_SECRET = "bqcweeocel5g75a";
     private Button mSubmit;
     private boolean mLoggedIn;
 
+    private String FileName = "MyWords1.txt";
 
+    private DropboxHelper dropboxHelper;
 
 
     @Override
@@ -74,36 +77,117 @@ public class MainActivity extends AppCompatActivity
         });
 
 
-        InitializeDropbox();
+        Log.i(TAG, "Token: " + RetriveAccessToken());
+
+        dropboxHelper = new DropboxHelper();
+
+        Toast.makeText(this, "Began, stored key: " + RetriveAccessToken(), Toast.LENGTH_SHORT).show();
+
+
 
 
     }
 
-    public void InitializeDropbox() {
+    boolean dropboxHasInitialized;
+
+
+    public DropboxAPI<AndroidAuthSession> getDropboxAPI()
+    {
+        // https://stackoverflow.com/questions/19083564/android-dropbox-api-not-saving-login
 
         AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
         AndroidAuthSession session = new AndroidAuthSession(appKeys);
         mDBApi = new DropboxAPI<AndroidAuthSession>(session);
-        mDBApi.getSession().startOAuth2Authentication(MainActivity.this);
+
+
+        String savedAccessToken = RetriveAccessToken();
+        if (savedAccessToken != "")
+            mDBApi.getSession().setOAuth2AccessToken(savedAccessToken);
+        else
+            mDBApi.getSession().startOAuth2Authentication(MainActivity.this);
+
+        return mDBApi;
+    }
+
+
+    public void InitializeDropbox(View view)
+    {
+
+        mDBApi = getDropboxAPI();
+        /*
+        AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeys);
+        mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+        //mDBApi.getSession().startOAuth2Authentication(MainActivity.this);
+
+
+        String savedAccessToken = RetriveAccessToken();
+        if (savedAccessToken != "")
+            mDBApi.getSession().setOAuth2AccessToken(savedAccessToken);
+        else
+            mDBApi.getSession().startOAuth2Authentication(MainActivity.this);
+
+
+
+
+        // https://stackoverflow.com/questions/19083564/android-dropbox-api-not-saving-login
+
+/*
+        AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeys);
+        mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+        //mDBApi.getSession().startOAuth2Authentication(MainActivity.this);
+
+        mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+
+        String savedAccessToken = RetriveAccessToken();
+
+        if (!TextUtils.isEmpty(savedAccessToken))
+        {
+            Log.i(TAG, "Using stored token");
+            mDBApi.getSession().setOAuth2AccessToken(savedAccessToken);
+        } else
+        {
+            mDBApi.getSession().startOAuth2Authentication(MainActivity.this);
+            Log.i(TAG, "Getting new token");
+
+        }
+
+
+        dropboxHasInitialized = true;
+
+        Log.i(TAG, "Dropbox successfully initialized!");
+        Toast.makeText(view.getContext(), "Dropbox successfully initialized!", Toast.LENGTH_SHORT).show();
+        */
+    }
+
+    public boolean isDropboxLinked()
+    {
+        Log.i(TAG, "Getting token ...");
+        return mDBApi != null && (mDBApi.getSession().isLinked() || mDBApi.getSession().authenticationSuccessful());
+    }
+
+    public void UploadDropbox(View view) throws DropboxException, IOException
+    {
+
+
+
+        Log.i(TAG, "Dropbox uploading...");
+        Toast.makeText(view.getContext(), "Uploading started...", Toast.LENGTH_SHORT).show();
+
+        dropboxHelper.UploadFile(FileName, this, getDropboxAPI());
+
 
     }
 
-    public void UploadDBText(View view) throws DropboxException, IOException
+    public void DownloadDropbox(View view) throws FileNotFoundException, DropboxException
     {
-        //InputStream input = this.getAssets().open("csv_text.txt");
-        //DropboxHelper.UploadFile(input,true);
 
 
-        String m = "/MyWords2.txt";
-        DropboxHelper.DownloadFile(m);
+        Log.i(TAG, "Dropbox downloading...");
+        Toast.makeText(view.getContext(), "Downloading started...", Toast.LENGTH_SHORT).show();
 
-
-    }
-
-    public void DownloadDBText(View view) throws FileNotFoundException, DropboxException
-    {
-        String m = "/MyWords2.txt";
-        DropboxHelper.DownloadFile(m);
+        dropboxHelper.DownloadFile(FileName, this, getDropboxAPI());
 
     }
 
@@ -137,6 +221,9 @@ public class MainActivity extends AppCompatActivity
     {
         super.onResume();
 
+        if (!dropboxHasInitialized)
+            return;
+
         if (mDBApi.getSession().authenticationSuccessful())
         {
             try
@@ -145,10 +232,34 @@ public class MainActivity extends AppCompatActivity
                 mDBApi.getSession().finishAuthentication();
 
                 String accessToken = mDBApi.getSession().getOAuth2AccessToken(); // TODO: save the accessToken for later use
+                StoreAccessToken(accessToken);
+                Log.i(TAG, "The new token: " + accessToken);
             } catch (IllegalStateException e)
             {
                 Log.i("DbAuthLog", "Error authenticating", e);
             }
         }
+    }
+
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+    final String AccessToken = "AccessToken";
+
+    private void StoreAccessToken(String token)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString(AccessToken, token);
+        editor.commit();
+
+    }
+
+    private String RetriveAccessToken()
+    {
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String restoredText = prefs.getString(AccessToken, null);
+
+        if (restoredText != null)
+            return restoredText;
+        else
+            return "";
     }
 }
