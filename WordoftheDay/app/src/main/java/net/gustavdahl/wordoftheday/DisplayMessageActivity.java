1b105dbe3cb5f5ee8ac0bf34eb3b92443f568ee3
Gustav Dahl <Linkthewindwaker@gmail.com>
2016-02-27 15:54:00 +0100

    Can write simple txt file

--- WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/DisplayMessageActivity.java
+++ WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/DisplayMessageActivity.java
@@ -1,11 +1,26 @@
 package net.gustavdahl.wordoftheday;
 
+import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class DisplayMessageActivity extends AppCompatActivity
 {
@@ -29,6 +44,68 @@ public class DisplayMessageActivity extends AppCompatActivity
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
+
+        textView.setText("Could write to documents: " + WriteToExternalStorage(message));
+
     }
+
+    private String WriteToExternalStorage(String text)
+    {
+        if (IsExternalStorageWritable())
+            return CreateExternalStoragePublicPicture(text);
+        else
+            return "Could not write file";
+    }
+
+    public boolean IsExternalStorageWritable()
+    {
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state))
+            return true;
+        else
+            return false;
+    }
+
+    public boolean IsExternalStorageReadable()
+    {
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state))
+            return true;
+        else
+            return false;
+    }
+
+    String CreateExternalStoragePublicPicture(String text)
+    {
+
+        File path = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS);
+
+        String filePath = path + "/1AndroidTest.txt";
+
+        try
+        {
+            BufferedWriter out = new BufferedWriter(new FileWriter(filePath));
+            out.write(text);
+            out.close();
+        } catch (IOException e)
+        {
+
+        }
+
+        return filePath;
+    }
+
 
 }
