119ee6fde397d2d5ce8f7c8e68186c439a952c73
Gustav Dahl <Linkthewindwaker@gmail.com>
2016-02-27 16:36:50 +0100

    Now with simple JSON writing/reading

--- WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/DisplayMessageActivity.java
+++ WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/DisplayMessageActivity.java
@@ -1,10 +1,9 @@
 package net.gustavdahl.wordoftheday;
 
 import android.content.Intent;
-import android.media.MediaScannerConnection;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,13 +13,16 @@ import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class DisplayMessageActivity extends AppCompatActivity
 {
@@ -53,17 +55,50 @@ public class DisplayMessageActivity extends AppCompatActivity
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
         layout.addView(textView);
 
-        textView.setText("Could write to documents: " + WriteToExternalStorage(message));
+        //textView.setText("Could write to documents: " + WriteToExternalStorage(message));
 
-    }
-
-    private String WriteToExternalStorage(String text)
-    {
         if (IsExternalStorageWritable())
-            return CreateExternalStoragePublicPicture(text);
+        {
+            WriteFile(message, GetFilePath());
+            try
+            {
+                SetText(textView, GetFilePath());
+            } catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
         else
-            return "Could not write file";
+            Log.i(this.toString(), "Could not write to " + GetFilePath());
+
     }
+
+    private void SetText(TextView textView, String filePath) throws JSONException
+    {
+        String read = "";
+        try
+        {
+            read = ReadFile(GetFilePath());
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        JSONObject json = new JSONObject(read);
+        String name = json.getString("name");
+        int score = json.getInt("score");
+
+        textView.setText("Name: " + name + "; Score: " + score);
+    }
+
+    private String GetFilePath()
+    {
+        File path = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS);
+
+        return path + "/1AndroidTest.txt";
+    }
+
 
     public boolean IsExternalStorageWritable()
     {
@@ -86,25 +121,69 @@ public class DisplayMessageActivity extends AppCompatActivity
             return false;
     }
 
-    String CreateExternalStoragePublicPicture(String text)
+    void WriteFile(String textToWrite, String filePath)
     {
-
-        File path = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DOWNLOADS);
-
-        String filePath = path + "/1AndroidTest.txt";
-
         try
         {
             BufferedWriter out = new BufferedWriter(new FileWriter(filePath));
-            out.write(text);
+            out.write(WriteJSON());
             out.close();
         } catch (IOException e)
         {
 
         }
+    }
 
-        return filePath;
+    public String ReadFile(String path) throws IOException
+    {
+        String text = "";
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try
+        {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null)
+            {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            text = sb.toString();
+        } finally
+        {
+            br.close();
+        }
+
+        return text;
+    }
+
+    public String ReadJSON(String input) throws JSONException
+    {
+        JSONObject json = new JSONObject(input);
+        Log.i(this.toString(), json.toString());
+
+        return "";
+    }
+
+    public String WriteJSON()
+    {
+        JSONObject object = new JSONObject();
+        JSONArray array = new JSONArray();
+
+        try
+        {
+            object.put("name", "Jack Hack");
+            object.put("score", new Integer(200));
+            object.put("current", new Double(152.32));
+            object.put("nickname", "Hacker");
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println(object);
+
+        return object.toString();
     }
 
 
