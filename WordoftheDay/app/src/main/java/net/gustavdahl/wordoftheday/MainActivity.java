63f691cbcda8dc9ed3965190c55f49ce996a9f4a
Gustav Dahl <Linkthewindwaker@gmail.com>
2016-03-08 21:37:12 +0100

    Now with simple time reminders

--- WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/MainActivity.java
+++ WordoftheDay/app/src/main/java/net/gustavdahl/wordoftheday/MainActivity.java
@@ -2,7 +2,13 @@
 
 package net.gustavdahl.wordoftheday;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +19,8 @@ import android.widget.Toast;
 import org.json.JSONException;
 
 import java.io.File;
+import java.util.Calendar;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -23,6 +31,8 @@ public class MainActivity extends AppCompatActivity
     private TextView meaningTextView;
     private RatingBar usedCountStars;
 
+    private Calendar calendar;
+
 
     private void InitializeTextViews()
     {
@@ -30,8 +40,6 @@ public class MainActivity extends AppCompatActivity
         meaningTextView = (TextView)findViewById(R.id.word_meaning);
         usedCountStars = (RatingBar)findViewById(R.id.usedCountStars);
     }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -52,7 +60,77 @@ public class MainActivity extends AppCompatActivity
 
         Word.setSelectedWord(Word.allActiveWordObjects.get(0));
         UpdateTextViews();
+
+        //TODO: save calendar as persitable
+        // https://developer.android.com/guide/components/activities.html
+
+        if (calendar == null)
+            calendar = GetRandomTime();
+
+        scheduleNotification(getNotification(Word.getSelectedWord().getWord()), calendar);
+        Toast.makeText(MainActivity.this, "Next time: " + calendar.getTime(), Toast.LENGTH_LONG).show();
+
     }
+
+    private Calendar GetRandomTime()
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+
+        int minMinute = 0;
+        int maxMinute = 60;
+
+        int minHour = 8;
+        int maxHour = 17;
+
+        Random random = new Random();
+
+        int minute = random.nextInt(maxMinute - minMinute + 1) + minMinute;
+        int hour = random.nextInt(maxHour - minHour + 1) + minHour;
+
+        int tries = 0;
+        while (hour < Calendar.getInstance().getTime().getHours() && tries < 10)
+        {
+            tries++;
+
+            if (tries < 10)
+                hour = random.nextInt(maxHour - minHour + 1) + minHour;
+            else // not sure if this works...
+                calendar.add(Calendar.DAY_OF_YEAR, 1);
+
+        }
+
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+
+        return calendar;
+    }
+
+    private void scheduleNotification(Notification notification, Calendar calendar) {
+
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+    }
+
+    private Notification getNotification(String content)
+    {
+        // https://gist.github.com/BrandonSmith/6679223
+        // https://stackoverflow.com/questions/3052149/using-alarmmanager-to-start-a-service-at-specific-time#3058837
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle(content);
+        builder.setContentText("Remember to use this word.");
+        builder.setSmallIcon(R.drawable.checkmark);
+        builder.setContentIntent(PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0));
+
+        return builder.build();
+    }
+
 
     private void CheckIfJsonFileExists()
     {
